94051f7b34b849a5704a7ee3a3452b64fc03e0cc
Matthew <mthanjan@tamu.edu>
2024-09-16 17:12:50 -0500

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <Adafruit_BNO08x.h>
+#include <HX711.h>
+#include <SdFat.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
